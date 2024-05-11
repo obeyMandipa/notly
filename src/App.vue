9d2b31870cfddec16d86b0f6f097cb3328c5f978
Rufaro O Mandipa <mandipaobey@gmail.com>
2024-05-11 21:48:00 +0200

    initializing the project and setting up the ui

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        
+    </header>
+
+    <main>
+        <div class="h-[630px] w-[500px] bg-red-200 p-2 m-auto flex flex-col space-y-2" >
+            <div class="icons h-[50px] w-[486px] bg-blue-300"></div>
+            <div class="">
+                <textarea name="" id="" class="resize-none w-[486px] h-[500px] p-4" ></textarea>
+            </div>
+            <div class="w-[70px] h-[40px] bg-blue-400 align-center "><button>Save</button></div>
+        </div>
+    </main>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+body{
+    padding: 20px;
+}
+
+</style>
