a0ba75b4fdfd2f4a959cfaa4c9b9b09c6ac35765
Rufaro O Mandipa <mandipaobey@gmail.com>
2024-05-12 19:02:42 +0200

    conneted subabse database

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,61 @@
     </header>
 
     <main>
-        <div class="h-[630px] w-[500px] bg-red-200 p-2 m-auto flex flex-col space-y-2" >
-            <div class="icons h-[50px] w-[486px] bg-blue-300"></div>
-            <div class="">
-                <textarea name="" id="" class="resize-none w-[486px] h-[500px] p-4" ></textarea>
+        
+        <div class="h-[540px] w-[500px]  p-2 m-auto flex flex-col space-y-2 bg-red-400  p-4" >
+            <div class="flex space-x-2">
+                <input type="text" placeholder="Title" class="h-[50px] w-[360px] border border-black p-2 text-[18px] rounded-xl" v-model="title">
+                <div class="icons h-[50px] w-[130px] flex justify-between ">
+                    <img src="" alt="" class="w-[48px] h-[48px] bg-gray-400 rounded-full">
+                    <img src="" alt="" class="w-[48px] h-[48px] bg-gray-400 rounded-full">
+                </div>
             </div>
-            <div class="w-[70px] h-[40px] bg-blue-400 align-center "><button>Save</button></div>
+            <div class="">
+                <textarea name="" id="" class="resize-none border border-black text-[18px] w-[470px] h-[380px] rounded-xl p-4" v-model="notes"></textarea>
+            </div>
+           <button @click="save()" class="bg-[#c4f5d3] hover:bg-[#095721] hover:text-white w-[50px] h-[50px] rounded-full text-center">Save</button>
         </div>
+        
+
     </main>
 </template>
 
 <script>
+  import { supabase } from '@/lib/supabaseClient'
+
+
+export default {
+    data() {
+        return{
+            title: '',
+            notes: '',
+
+        };
+    },
+
+    Methods: {
+        async save() {
+            console.log('working')
+            const {data, error} = await supabase.from('Notes').insert('Title', 'Note')
+
+            if(error){
+                console.log(error, 'insertion was not successful')
+            }
+            else{
+                console.log(data)
+            }
+        }
+    }
+
+
+
+}
 
 </script>
 
 <style scoped>
-body{
-    padding: 20px;
-}
+/* main{
+    background-color: #c4f5d3;
+} */
 
 </style>
---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://kotytonjvjhnddqrdhhd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvdHl0b25qdmpobmRkcXJkaGhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1MzE5MzIsImV4cCI6MjAzMTEwNzkzMn0.I1Olz4W1O8SXe2FQrvmw9gaFJQMAprfKLPANp3V7eeI')
