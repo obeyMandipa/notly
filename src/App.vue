550f72b05836b7e4ec374834e305232ee6f7a1a7
Rufaro O Mandipa <mandipaobey@gmail.com>
2024-05-15 12:29:27 +0200

    added some icons

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,22 @@
 
     <main>
         
-        <div class="h-[540px] w-[500px]  p-2 m-auto flex flex-col space-y-2 bg-red-400  p-4" >
+        <div class="h-[540px] w-[500px]  p-2 m-auto flex flex-col space-y-2 bg-blue-100  p-4" >
             <div class="flex space-x-2">
-                <input type="text" placeholder="Title" class="h-[50px] w-[360px] border border-black p-2 text-[18px] rounded-xl" v-model="title">
+                <input type="text" placeholder="Title" class="h-[50px] w-[400px] border border-black p-2 text-[18px] rounded-xl" v-model="title">
                 <div class="icons h-[50px] w-[130px] flex justify-between ">
-                    <img src="" alt="" class="w-[48px] h-[48px] bg-gray-400 rounded-full">
-                    <img src="" alt="" class="w-[48px] h-[48px] bg-gray-400 rounded-full">
+                   <button class="w-[48px] h-[48px] bg-white rounded-full flex justify-center items-center"><img src="@/assets/icons/notes.png" alt="" class="w-[30px] h-[30px] "></button> 
+                   <button class="w-[48px] h-[48px] bg-white rounded-full flex justify-center items-center"><img src="@/assets/icons/settings.png" alt="" class="w-[30px] h-[30px] "></button> 
                 </div>
             </div>
             <div class="">
                 <textarea name="" id="" class="resize-none border border-black text-[18px] w-[470px] h-[380px] rounded-xl p-4" v-model="notes"></textarea>
             </div>
-           <button @click="save" class="bg-[#c4f5d3] hover:bg-[#095721] hover:text-white w-[50px] h-[50px] rounded-full text-center">Save</button>
+            <div class="flex justify-between "> 
+                <button @click="save" class="w-[50px] h-[50px] bg-[#ffffff] rounded-full flex justify-center items-center "><img src="@/assets/icons/save.png" alt="" class="w-[30px] h-[30px]"> </button>
+                <button @click="save" class="w-[50px] h-[50px] bg-[#ffffff] rounded-full flex justify-center items-center "><img src="@/assets/icons/pin.png" alt="" class="w-[28px] h-[28px]"> </button>
+
+            </div>
         </div>
         
 
